b0c8468be16143bf0b7f515aee135af46f92a2ca
smart-rabbit-studio <minug984@gmail.com>
2025-03-10 16:05:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+function showPage(pageId) {
+    document.querySelectorAll('.content').forEach(div => div.classList.add('hidden'));
+    document.getElementById(pageId).classList.remove('hidden');
+}
